b76c03be26de039ef6bed046735b899eaf86b7fb
shige15 <gabrielalfarezf15@gmail.com>
2025-09-30 20:35:33 +0700

    fix error

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -3,9 +3,9 @@ import SkillsSection from "../components/SkillSection";
 import ProjectsSection from "../components/ProjectSection";
 import ContactSection from "../components/ContactSection";
 import { Box } from "@mui/material";
-import LandingPage from "../Components/LandingPage";
-import Header from "../Components/Header";
-import Experience from "../Components/Experience";
+import LandingPage from "../components/LandingPage";
+import Header from "../components/Header";
+import Experience from "../components/Experience";
 import FadeSection from "../Style/FadeSection";
 
 export default function Home() {
